9f5e87a76b9c35d7d63b80dd5041a3bebd0167ab
kuntabruno <kuntabruno@gmail.com>
2022-10-23 09:23:06 +0300

    feat: Framework main entry file

---
+++ app/index.ts
@@ -0,0 +1,27 @@
+import { Application } from './application';
+import { AppEvent } from './application-events';
+import { Controller } from './controller';
+import { ModelSettings } from './decorators/model-settings';
+import { ViewSettings } from './decorators/view-settings';
+import { Dispatcher } from './dispatcher';
+import { EventEmitter } from './event-emitter';
+import { Mediator } from './mediator';
+import { Model } from './model';
+import { Route } from './route';
+import { Router } from './router';
+import { View } from './view';
+
+export {
+  Application,
+  AppEvent,
+  Controller,
+  ModelSettings,
+  ViewSettings,
+  Dispatcher,
+  EventEmitter,
+  Mediator,
+  Model,
+  Route,
+  Router,
+  View,
+};
